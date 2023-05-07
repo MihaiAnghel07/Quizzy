546715b68901cfd45ff583ad7203c1422b8da470
AndreiOstahie <andreiostahie@yahoo.com>
2023-05-07 15:41:33 +0300

    Updated sidebar, updated dashboard buttons

--- frontend-quizzy/src/components/MySidebar/MySidebar.js
+++ frontend-quizzy/src/components/MySidebar/MySidebar.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import './MySidebar.css'
-import logo from '../../assets/logo.jpg'
+import React, { useState } from 'react';
+import './MySidebar.css';
+import logo from '../../assets/logo.jpg';
 import { FaHome, FaQuestion } from 'react-icons/fa';
 import { VscAccount } from 'react-icons/vsc';
 import { GrContactInfo, GrContact } from 'react-icons/gr';
@@ -9,49 +9,78 @@ import Dashboard from '../../pages/Dashboard/Dashboard';
 import { useAuthContext } from '../../hooks/useAuthContext';
 import Account from '../../pages/Account/Account';
 import { AiOutlineMail } from 'react-icons/ai';
-import { CiLogout } from 'react-icons/ci'
-import { BsTelephoneFill } from 'react-icons/bs'
+import { CiLogout } from 'react-icons/ci';
+import { BsTelephoneFill } from 'react-icons/bs';
 import { useLogout } from '../../hooks/useLogout';
 
-
-
 export default function MySidebar() {
-
-  const { user } = useAuthContext()
+  const { user } = useAuthContext();
   const navigate = useNavigate();
-  const { logout } = useLogout()
+  const { logout } = useLogout();
+
+  const [selectedButton, setSelectedButton] = useState('dashboard');
 
   function handleDashboardButtonClick() {
+    setSelectedButton('dashboard');
     navigate('/dashboard');
   }
 
   function handleAccountButtonClick() {
+    setSelectedButton('account');
     navigate('/account');
   }
 
   function handleFaqButtonClick() {
+    setSelectedButton('faq');
     navigate('/faq');
   }
 
   function handleContactButtonClick() {
+    setSelectedButton('contact');
     navigate('/contact');
   }
 
-
   return (
-    <div className='mySidebar-wrapper'>
+    <div className="mySidebar-wrapper">
       <div>
-          <img className="mySidebar-logo" src={logo}/>
+        <img className="mySidebar-logo" src={logo} />
       </div>
-      <div className='sidebar-items-container'>
-        <div className='sidebar-main-buttons'>
-          <button id='sidebar-dashboard-button' onClick={handleDashboardButtonClick}><FaHome/>Dashboard</button>
-          <button id='sidebar-account-button' onClick={handleAccountButtonClick}><VscAccount/>Account</button>
-          <button id='sidebar-faq-button' onClick={handleFaqButtonClick}><FaQuestion/>FAQ</button>
-          <button id='sidebar-contact-button' onClick={handleContactButtonClick}><BsTelephoneFill/>Contact</button>
+      <div className="sidebar-items-container">
+        <div className="sidebar-main-buttons">
+          <button
+            id={selectedButton === 'dashboard' ? 'sidebar-dashboard-button-selected' : 'sidebar-dashboard-button'}
+            onClick={handleDashboardButtonClick}
+          >
+            <FaHome />
+            Dashboard
+          </button>
+          <button
+            id={selectedButton === 'account' ? 'sidebar-account-button-selected' : 'sidebar-account-button'}
+            onClick={handleAccountButtonClick}
+          >
+            <VscAccount />
+            Account
+          </button>
+          <button
+            id={selectedButton === 'faq' ? 'sidebar-faq-button-selected' : 'sidebar-faq-button'}
+            onClick={handleFaqButtonClick}
+          >
+            <FaQuestion />
+            FAQ
+          </button>
+          <button
+            id={selectedButton === 'contact' ? 'sidebar-contact-button-selected' : 'sidebar-contact-button'}
+            onClick={handleContactButtonClick}
+          >
+            <BsTelephoneFill />
+            Contact
+          </button>
         </div>
-        <button id='sidebar-logout-button' onClick={logout}><CiLogout/>Log out</button>
+        <button id="sidebar-logout-button" onClick={logout}>
+          <CiLogout />
+          Log out
+        </button>
       </div>
     </div>
-  )
+  );
 }
